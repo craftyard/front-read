8ac52ead9d2bcd0671921b03643fdc102cccbb01
FredSteady23 <beisenov_12@mail.ru>
2023-10-20 12:39:22 +0500

    'добавлены eslint and gitignore'

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
