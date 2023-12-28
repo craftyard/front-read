0ea38477e76e450d8f5d9dc93419001bd453dace
xHetso <datacener23491248@gmail.com>
2023-12-28 12:15:25 +0500

    Пофиксил ошибки

---
+++ src/workshop/services/find-workshop-by-user-id/use-case.test.ts
@@ -0,0 +1,198 @@
+import {
+  describe, test, expect, spyOn,
+} from 'bun:test';
+import { ConsoleLogger } from 'rilata/src/common/logger/console-logger';
+import { GetMyWorkshopActionDod, GetMyWorkshopOut, WorkshopForUserDoesntExistError } from 'cy-domain/src/workshop/domain-data/workshop/get-my-workshop/s-params';
+import { Logger } from 'rilata/src/common/logger/logger';
+import { WorkshopRepository } from 'cy-domain/src/workshop/domain-object/workshop/repository';
+import { WorkshopAttrs } from 'cy-domain/src/workshop/domain-data/workshop/params';
+import { Database } from 'rilata/src/app/database';
+import { ModuleResolver } from 'rilata/src/app/resolves/module-resolver';
+import { TokenVerifier } from 'rilata/src/app/jwt/token-verifier.interface';
+import { DTO } from 'rilata/src/domain/dto';
+import { Module } from 'rilata/src/app/module/module';
+import { AnonymousUser, DomainUser } from 'rilata/src/app/caller';
+import { UuidType } from 'rilata/src/common/types';
+import { StorePayload, ThreadStore } from 'rilata/src/app/async-store/types';
+import { storeDispatcher } from 'rilata/src/app/async-store/store-dispatcher';
+import { FindWorkshopByUserIdUC } from './use-case';
+
+export class WorkshopRepoMock implements WorkshopRepository {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findWorkshopByUserId(userId: string): Promise<WorkshopAttrs | undefined> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+export class ResolverMock implements ModuleResolver {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  init(module: Module): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getModule(): Module {
+    throw new Error('Method not implemented.');
+  }
+
+  private repoMock = new WorkshopRepoMock();
+
+  getLogger(): Logger {
+    return new ConsoleLogger();
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getRepository(repoKey: unknown): WorkshopRepository {
+    return this.repoMock;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getRealisation(...args: unknown[]): unknown {
+    throw new Error('Method not implemented.');
+  }
+
+  getDatabase(): Database {
+    throw new Error('Method not implemented.');
+  }
+
+  getTokenVerifier(): TokenVerifier<DTO> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+const domainUser: DomainUser = {
+  type: 'DomainUser',
+  userId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d8374',
+};
+
+const moduleResolver: ModuleResolver = new ResolverMock();
+
+const actionId: UuidType = 'pb8a83cf-25a3-2b4f-86e1-2744de6d8374';
+
+const domainUserStorePayload: StorePayload = {
+  caller: domainUser,
+  moduleResolver,
+  actionId,
+};
+
+const domainUserThreadStore: ThreadStore<StorePayload> = {
+  run: () => {
+    throw new Error('Method not implemented.');
+  },
+  getStore: () => domainUserStorePayload,
+};
+
+const anonymousUser:AnonymousUser = {
+  type: 'AnonymousUser',
+};
+
+const anonymousUserStorePayload: StorePayload = {
+  caller: anonymousUser,
+  moduleResolver,
+  actionId,
+};
+
+const anonymousUserThreadStore: ThreadStore<StorePayload> = {
+  run: () => {
+    throw new Error('Method not implemented.');
+  },
+  getStore: () => anonymousUserStorePayload,
+};
+
+describe('Тесты для use-case getMyWorkshop', () => {
+  const sut = new FindWorkshopByUserIdUC();
+  const resolver = new ResolverMock();
+  sut.init(resolver);
+
+  const validInputOptions: GetMyWorkshopActionDod = {
+    attrs: {
+    },
+    meta: {
+      name: 'getMyWorkshop',
+      actionId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d8374',
+      domainType: 'action',
+    },
+  };
+
+  const invalidUserIdsInWorkshop : GetMyWorkshopActionDod = {
+    attrs: {
+    },
+    meta: {
+      name: 'getMyWorkshop',
+      actionId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d4444',
+      domainType: 'action',
+    },
+  };
+  const anonymousInputOptions : GetMyWorkshopActionDod = {
+    attrs: {
+    },
+    meta: {
+      name: 'getMyWorkshop',
+      actionId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d5555',
+      domainType: 'action',
+    },
+  };
+
+  const workshop: (WorkshopAttrs) = {
+    workshopId: '6f91d305-3f4b-4a3d-9bef-72cf3757cc33',
+    name: 'Nurbolat',
+    city: 'Freital',
+    address: 'Gerti-Bruns-Weg 4/7 70279 Freital',
+    location: { latitude: 88.958285, longitude: 117.84182 },
+    employeesRole: { userIds: ['fb8a83cf-25a3-2b4f-86e1-27f6de6d8374', '3312a8d6-67ab-4e87-8a21-9d17f508fd5c'] },
+  };
+
+  test('успех, запрос для получения workshop-а успешно проходит', async () => {
+    const getWorkshopMock = spyOn(
+      resolver.getRepository('repoKey'),
+      'findWorkshopByUserId',
+    ).mockResolvedValueOnce(workshop);
+    storeDispatcher.setThreadStore(domainUserThreadStore);
+    const result = await sut.execute(validInputOptions);
+    expect(result.isSuccess()).toBe(true);
+    expect(result.value as GetMyWorkshopOut).toEqual(workshop);
+    expect(getWorkshopMock).toHaveBeenCalledTimes(1);
+    expect(getWorkshopMock.mock.calls[0][0]).toBe('fb8a83cf-25a3-2b4f-86e1-27f6de6d8374');
+  });
+
+  test('провал, для текущего пользователя мастерская не найдена', async () => {
+    const getWorkshopMock = spyOn(
+      resolver.getRepository('repoKey'),
+      'findWorkshopByUserId',
+    ).mockResolvedValueOnce(undefined);
+    storeDispatcher.setThreadStore(domainUserThreadStore);
+    const result = await sut.execute(invalidUserIdsInWorkshop);
+    expect(result.isFailure()).toBe(true);
+    expect(result.value as WorkshopForUserDoesntExistError).toEqual({
+      locale: {
+        text: 'Мастерская не найдена',
+        hint: {},
+      },
+      meta: {
+        name: 'WorkshopForUserDoesntExistError',
+        domainType: 'error',
+        errorType: 'domain-error',
+      },
+    });
+    expect(getWorkshopMock).toHaveBeenCalledTimes(2);
+    expect(getWorkshopMock.mock.calls[0][0]).toBe('fb8a83cf-25a3-2b4f-86e1-27f6de6d8374');
+  });
+
+  test('провал, запрещен доступ неавторизованному пользователю', async () => {
+    storeDispatcher.setThreadStore(anonymousUserThreadStore);
+    const result = await sut.execute(anonymousInputOptions);
+    expect(result.isFailure()).toBe(true);
+    expect(result.value).toEqual({
+      locale: {
+        text: 'Действие не доступно',
+        hint: {
+          allowedOnlyFor: ['DomainUser'],
+        },
+      },
+      meta: {
+        name: 'Permission denied',
+        errorType: 'domain-error',
+        domainType: 'error',
+      },
+    });
+  });
+});
