d6edd43e9c50bf0587d57b91672a46b19c9290c1
xHetso <datacener23491248@gmail.com>
2024-02-08 12:21:47 +0500

    Пофиксил ошибки

--- src/subject-workshop/services/workshop/find-workshop-by-user-id/fixture.ts
+++ src/subject-workshop/services/workshop/find-workshop-by-user-id/fixture.ts
@@ -1,4 +1,4 @@
-import { GetMyWorkshopActionDod } from 'cy-domain/src/workshop/domain-data/workshop/get-my-workshop/s-params';
+import { FindWorkshopByUserIdActionDod } from 'cy-domain/src/workshop/domain-data/workshop/find-workshop-by-user-id/s-params';
 import { WorkshopAttrs } from 'cy-domain/src/workshop/domain-data/workshop/params';
 import { WorkshopReadRepository } from 'cy-domain/src/workshop/domain-object/workshop/repository';
 import { ModuleResolver } from 'rilata/src/app/resolves/module-resolver';
@@ -10,7 +10,6 @@ import { UserAR } from 'cy-domain/src/subject/domain-object/user/a-root';
 import { UserAttrs } from 'cy-domain/src/subject/domain-data/user/params';
 import { UserDoesNotExistError } from 'cy-domain/src/subject/domain-data/user/get-user/s-params';
 import { Result } from 'rilata/src/common/result/types';
-import { Repositoriable } from 'rilata/src/app/resolves/repositoriable';
 
 export class WorkshopRepoMock implements WorkshopReadRepository {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -48,11 +47,12 @@ export const resolverGetUserWorkshopRepoMock = spyOn(
 }) as Mock<(...args: unknown[]) => UserRepoMock | WorkshopRepoMock>;
 
 
-export const inputOptions : GetMyWorkshopActionDod = {
+export const inputOptions : FindWorkshopByUserIdActionDod = {
   attrs: {
+    userId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d8374',
   },
   meta: {
-    name: 'getMyWorkshop',
+    name: 'findWorkshopByUserId',
     actionId: 'fb8a83cf-25a3-2b4f-86e1-27f6de6d4444',
     domainType: 'action',
   },
--- src/subject-workshop/services/workshop/find-workshop-by-user-id/service.ts
+++ src/subject-workshop/services/workshop/find-workshop-by-user-id/service.ts
@@ -1,8 +1,8 @@
 import { QueryService } from 'rilata/src/app/service/query-service';
 import { ServiceResult } from 'rilata/src/app/service/types';
-import { GetMyWorkshopActionDod, GetMyWorkshopServiceParams, WorkshopForUserDoesntExistError } from 'cy-domain/src/workshop/domain-data/workshop/get-my-workshop/s-params';
+import { FindWorkshopByUserIdActionDod, FindWorkshopByUserIdServiceParams, WorkshopForUserDoesntExistError } from 'cy-domain/src/workshop/domain-data/workshop/find-workshop-by-user-id/s-params';
 import { WorkshopReadRepository } from 'cy-domain/src/workshop/domain-object/workshop/repository';
-import { getMyWorkshopValidator } from 'cy-domain/src/workshop/domain-data/workshop/get-my-workshop/s-vmap';
+import { findWorkshopByUserIdValidator } from 'cy-domain/src/workshop/domain-data/workshop/find-workshop-by-user-id/s-vmap';
 import { success } from 'rilata/src/common/result/success';
 import { DomainUser } from 'rilata/src/app/caller';
 import { failure } from 'rilata/src/common/result/failure';
@@ -10,19 +10,19 @@ import { dodUtility } from 'rilata/src/common/utils/domain-object/dod-utility';
 import { storeDispatcher } from 'rilata/src/app/async-store/store-dispatcher';
 import { UserReadRepository } from 'cy-domain/src/subject/domain-object/user/read-repository';
 
-export class FindWorkshopByUserIdService extends QueryService<GetMyWorkshopServiceParams> {
+export class FindWorkshopByUserIdService extends QueryService<FindWorkshopByUserIdServiceParams> {
   protected aRootName: 'WorkshopAR' = 'WorkshopAR' as const;
 
-  protected name: 'getMyWorkshop' = 'getMyWorkshop' as const;
+  protected name: 'findWorkshopByUserId' = 'findWorkshopByUserId' as const;
 
   protected supportedCallers: readonly ('ModuleCaller' | 'AnonymousUser' | 'DomainUser')[] = ['DomainUser'];
 
-  protected validatorMap = getMyWorkshopValidator;
+  protected validatorMap = findWorkshopByUserIdValidator;
 
   protected async runDomain(
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    actionDod: GetMyWorkshopActionDod,
-  ): Promise<ServiceResult<GetMyWorkshopServiceParams>> {
+    actionDod: FindWorkshopByUserIdActionDod,
+  ): Promise<ServiceResult<FindWorkshopByUserIdServiceParams>> {
     const repoWorkshop = WorkshopReadRepository.instance(this.moduleResolver);
     const { caller } = storeDispatcher.getStoreOrExepction();
     const workshop = await repoWorkshop.findWorkshopByUserId((caller as DomainUser).userId);
