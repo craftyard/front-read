178466eccd957394bd1d3fca3c264954f31e2d3d
Hetso <datacener23491248@gmail.com>
2024-02-12 15:34:55 +0500

    Пофиксил ошибки

--- src/subject-workshop/services/workshop/find-workshop-by-user-id/service.ts
+++ src/subject-workshop/services/workshop/find-workshop-by-user-id/service.ts
@@ -1,11 +1,9 @@
 import { QueryService } from 'rilata/src/app/service/query-service';
 import { ServiceResult } from 'rilata/src/app/service/types';
-import { FindWorkshopByUserIdActionDod, FindWorkshopByUserIdServiceParams, WorkshopForUserDoesntExistError } from 'cy-domain/src/workshop/domain-data/workshop/find-workshop-by-user-id/s-params';
+import { FindWorkshopByUserIdActionDod, FindWorkshopByUserIdServiceParams } from 'cy-domain/src/workshop/domain-data/workshop/find-workshop-by-user-id/s-params';
 import { WorkshopReadRepository } from 'cy-domain/src/workshop/domain-object/workshop/repository';
 import { findWorkshopByUserIdValidator } from 'cy-domain/src/workshop/domain-data/workshop/find-workshop-by-user-id/s-vmap';
 import { success } from 'rilata/src/common/result/success';
-import { failure } from 'rilata/src/common/result/failure';
-import { dodUtility } from 'rilata/src/common/utils/domain-object/dod-utility';
 import { UserReadRepository } from 'cy-domain/src/subject/domain-object/user/read-repository';
 
 export class FindWorkshopByUserIdService extends QueryService<FindWorkshopByUserIdServiceParams> {
@@ -24,11 +22,7 @@ export class FindWorkshopByUserIdService extends QueryService<FindWorkshopByUser
     const repoWorkshop = WorkshopReadRepository.instance(this.moduleResolver);
     const workshop = await repoWorkshop.findWorkshopByUserId(actionDod.attrs.userId);
     if (!workshop) {
-      return failure(dodUtility.getDomainErrorByType<WorkshopForUserDoesntExistError>(
-        'WorkshopForUserDoesntExistError' as const,
-        'Мастерская не найдена',
-        {},
-      ));
+      return success(undefined);
     }
     const repoUsers = UserReadRepository.instance(this.moduleResolver);
     const usersAttrs = await repoUsers.getUsers(workshop.employeesRole.userIds);
