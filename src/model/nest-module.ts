a0cb42ef75e0e3be060594d3f1c0e46f8cf044e3
Azat <teronarray@gmail.com>
2024-02-14 15:22:05 +0500

    done

---
+++ src/model/nest-module.ts
@@ -0,0 +1,34 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { JSONWebTokenLibJWTManager } from 'backend-core/src/infra/jwt/jsonwebtoken-lib.jwt.manager';
+import { RunMode } from 'rilata/src/app/types';
+import { Logger } from 'rilata/src/common/logger/logger';
+import { ModelReadResolver } from './resolver';
+import { ModelReadModule } from './module';
+import { ModelReadController } from './controllers/controller';
+
+@Module({})
+export class ModelReadNestModule {
+  static forRoot(
+    logger: Logger,
+    jwtManager: JSONWebTokenLibJWTManager,
+    runMode: RunMode,
+  ): DynamicModule {
+    const modelReadResolver = new ModelReadResolver(
+      jwtManager,
+      new ModelReadRepository(),
+      logger,
+      runMode,
+    );
+    return {
+      module: ModelReadNestModule,
+      providers: [
+        ModelReadModule,
+        {
+          provide: ModelReadResolver,
+          useValue: modelReadResolver,
+        },
+      ],
+      controllers: [ModelReadController],
+    };
+  }
+}
