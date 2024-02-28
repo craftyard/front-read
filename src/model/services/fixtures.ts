000ad67302a3279d377222c8e46b882d3002e986
Hetso <datacener23491248@gmail.com>
2024-02-28 15:16:07 +0500

    Пофиксил ошибки eslint tslint

--- src/model/services/fixtures.ts
+++ src/model/services/fixtures.ts
@@ -1,12 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Mock, spyOn } from 'bun:test';
+import { GettingWorkshopModelServiceParams } from 'cy-domain/src/model/domain-data/model/get-model/s-params';
 import { GetWorkshopModelsActionDod } from 'cy-domain/src/model/domain-data/model/get-models/s-params';
 import { ModelAttrs } from 'cy-domain/src/model/domain-data/params';
 import { ModelReadRepository } from 'cy-domain/src/model/domain-object/model/read-repository';
+import { ServiceResult } from 'rilata/src/app/service/types';
 import { UuidType } from 'rilata/src/common/types';
 import { resolver } from 'rilata/tests/fixtures/test-resolver-mock';
 
 export namespace ModelServiceFixtures {
     export class ModelReadRepositoryMock implements ModelReadRepository {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      getWorkshopModel(workshopId: string, modelId: string): Promise<
+      ServiceResult<GettingWorkshopModelServiceParams>> {
+        throw new Error('Method not implemented.');
+      }
+
       getWorkshopModels(workshopId: string): Promise<ModelAttrs[]> {
         throw new Error('Method not implemented.');
       }
@@ -39,18 +48,21 @@ export namespace ModelServiceFixtures {
         workshopId: 'e59725e7-39ae-48cd-aa10-a0f9a00c0fd9',
         name: 'Тубаретка',
         category: 'Мебель',
+        images: [],
       },
       {
         modelId: '63f0cc04-5b00-4b4b-a098-37a5d8afe38f',
         workshopId: 'e59725e7-39ae-48cd-aa10-a0f9a00c0fd9',
         name: 'Нож',
         category: 'Кухонная утварь',
+        images: [],
       },
       {
         modelId: '6ccb8dc3-90d3-4e8e-b084-b3300c3e8512',
         workshopId: 'e59725e7-39ae-48cd-aa10-a0f9a00c0fd9',
         name: 'Машинка',
         category: 'Игрушки',
+        images: [],
       },
     ];
 }
